e973f06dc4b311128b7800e4128f5f6e9d99a26f
takuowake <takuowake@gmail.com>
2023-05-29 16:07:34 +0900

    モデルの定義

--- lib/model/freezed/plan_model.dart
+++ lib/model/freezed/plan_model.dart
@@ -1,1 +1,28 @@
 /// カレンダー画面で使うプロバイダのためのクラスを記述
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../db/plan_db.dart';
+
+part 'plan_model.freezed.dart';
+
+@freezed
+//このクラスは、DBの状態を保持するクラスです。
+abstract class PlanStateData with _$PlanStateData {
+  factory PlanStateData({
+    @Default(false) bool isLoading,
+    @Default(false) bool isReadyData,
+    @Default([]) List<PlanItemData> planItems,
+  }) = _PlanStateData;
+}
+
+@freezed
+//このクラスは、入力中のplanを保持するクラスです。
+abstract class TempPlanItemData with _$TempPlanItemData {
+  factory TempPlanItemData({
+    @Default('') String title,
+    @Default('') String comment,
+    @Default(false) bool isAllDay,
+    @Default(null) DateTime? start,
+    @Default(null) DateTime? end,
+  }) = _TempPlanItemData;
+}
