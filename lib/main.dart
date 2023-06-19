ea60271218720850bf93e19d25590e0f9d08c824
takuowake <takuowake@gmail.com>
2023-06-20 07:20:31 +0900

    Device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,14 @@
-import 'package:calender_app/routing/router.dart' as Router;
+import 'package:calender_app/routing/router.dart' as router;
 import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// 通常のRun
 // void main() {
 //   runApp(const ProviderScope(child: MyApp()));
 // }
-void main() {
-  runApp(
-    DevicePreview(
-      enabled: true, // Toggle this flag for testing on a physical device
-      builder: (context) => ProviderScope(child: MyApp()),
-    ),
-  );
-}
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}): super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: DevicePreview.locale(context), // Add the locale here
-      builder: DevicePreview.appBuilder, // Add the builder here
-      title: 'カレンダー',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      onGenerateRoute: Router.Router.generateRoute,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('ja', 'JP'),
-      ],
-    );
-  }
-}
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({Key? key}): super(key: key);
@@ -66,3 +34,39 @@ class MyApp extends StatelessWidget {
 //     );
 //   }
 // }
+
+
+/// Device Preview
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true, // Toggle this flag for testing on a physical device
+      builder: (context) => ProviderScope(child: MyApp()),
+    ),
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
+      title: 'カレンダー',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: router.Router.generateRoute,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja', 'JP'),
+      ],
+    );
+  }
+}
