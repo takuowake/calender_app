057a8ad997ec4f245b28a869a7f099843bf0301d
takuowake <takuowake@gmail.com>
2023-06-20 07:14:28 +0900

    Device Preview導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
 import 'package:calender_app/routing/router.dart' as Router;
+import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// void main() {
+//   runApp(const ProviderScope(child: MyApp()));
+// }
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    DevicePreview(
+      enabled: true, // Toggle this flag for testing on a physical device
+      builder: (context) => ProviderScope(child: MyApp()),
+    ),
+  );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
       title: 'カレンダー',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/',
       onGenerateRoute: Router.Router.generateRoute,
-      locale: const Locale('ja', 'JP'),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
@@ -32,3 +41,28 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}): super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'カレンダー',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       initialRoute: '/',
+//       onGenerateRoute: Router.Router.generateRoute,
+//       locale: const Locale('ja', 'JP'),
+//       localizationsDelegates: const [
+//         GlobalMaterialLocalizations.delegate,
+//         GlobalWidgetsLocalizations.delegate,
+//         GlobalCupertinoLocalizations.delegate,
+//       ],
+//       supportedLocales: const [
+//         Locale('ja', 'JP'),
+//       ],
+//     );
+//   }
+// }
