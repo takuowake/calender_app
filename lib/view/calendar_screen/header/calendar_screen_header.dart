be4444925b0208c35f3aa1cfe66b16c5e44e5429
takuowake <takuowake@gmail.com>
2023-06-20 07:13:57 +0900

    importの変更

--- lib/view/calendar_screen/header/calendar_screen_header.dart
+++ lib/view/calendar_screen/header/calendar_screen_header.dart
@@ -1,5 +1,5 @@
 import 'package:calender_app/common/string.dart';
-import 'package:calender_app/repository/view_model/plan_provider.dart';
+import 'package:calender_app/repository/providers/plan_provider.dart';
 import 'package:calender_app/view/calendar_screen/header/header_datepicker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
